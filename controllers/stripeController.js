e3e39d86c87fa4efe60dd2adde537dcc08a925de
raza <razaaly0007@gmail.com>
2021-12-09 17:13:35 +0500

    initial commit

---
+++ controllers/stripeController.js
@@ -0,0 +1,18 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+const stripeController = async (req, res) => {
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee + purchase;
+  };
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+  });
+
+  res.json({ clientSecret: paymentIntent.client_secret, paymentIntent });
+};
+
+module.exports = stripeController;
